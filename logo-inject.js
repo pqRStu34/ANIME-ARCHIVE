d6f1bb745006be2c565e13f08cc2ee9f522b64eb
pqRStu34 <thrajsingha11@gmail.com>
2025-06-23 03:20:35 +0530

    update1icon once more

--- logo-inject.js
+++
@@ -1,8 +0,0 @@
-// This script injects the website logo into the element with id 'logo'.
-window.addEventListener("DOMContentLoaded", function () {
-  var logoContainer = document.getElementById("logo");
-  if (logoContainer) {
-    logoContainer.innerHTML =
-      '<img src="weblogo.png" alt="Website Logo" style="height:60px;">';
-  }
-});
